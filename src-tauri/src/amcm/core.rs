a26185e876f49e3995e42933bf09751ad711bc5e
Azur冰弦 <973562770@qq.com>
2022-12-25 22:20:04 +0800

    Clean up

--- src-tauri/src/amcm/core.rs
+++ src-tauri/src/amcm/core.rs
@@ -5,10 +5,9 @@ use crate::CORE;
 use std::env::current_exe;
 use std::fs;
 use std::io::{Read, Write};
-use std::path::{PathBuf, Path};
+use std::path::{Path, PathBuf};
 use std::time::Instant;
 
-use hotwatch::{Event, Hotwatch};
 use notify::{recommended_watcher, RecommendedWatcher, RecursiveMode, Watcher};
 use tauri::Window;
 pub struct Core {
@@ -20,7 +19,6 @@ pub struct Core {
     data_path: PathBuf,
     storge_dir: PathBuf,
 
-    hotwatch_watcher: Hotwatch,
     notify_watcher: Option<RecommendedWatcher>,
 }
 
@@ -34,7 +32,6 @@ impl Core {
             data_path: amcm_dir.join("amcm-data.json"),
             storge_dir: amcm_dir.join("storage/"),
             amcm_dir,
-            hotwatch_watcher: Hotwatch::new().expect("hotwatch failed to initialize!"),
             notify_watcher: None,
         }
     }
@@ -144,136 +141,108 @@ impl Core {
         res
     }
 
-    fn notify_event_fn(res: notify::Result<notify::Event>) {}
+    fn notify_event_handler(res: notify::Result<notify::Event>, window: Window) {
+        use notify::{
+            event::{CreateKind, ModifyKind, RemoveKind, RenameMode},
+            EventKind,
+        };
+        match res {
+            Ok(event) => {
+                println!("###### Notify Event handle start #####");
+
+                // Get amcm lock
+                let time_event_start = Instant::now();
+                let mut amcm = futures::executor::block_on(async { CORE.lock().await });
+                println!("get amcm lock cost: {:#?}", time_event_start.elapsed());
+
+                // Update mod file
+                let time_start = Instant::now();
+                println!("{:#?}", event);
+                let path = event.paths.last().unwrap();
+                let kind = event.kind;
+                if let EventKind::Create(create_kind) = kind {
+                    if let CreateKind::File = create_kind {
+                        amcm.data()
+                            .update_mod_file(String::from(path.to_str().unwrap()));
+                    } else if let CreateKind::Any = create_kind {
+                        amcm.data()
+                            .update_mod_file(String::from(path.to_str().unwrap()));
+                    }
+                } else if let EventKind::Modify(modify_kind) = kind {
+                    if let ModifyKind::Name(rename_mode) = modify_kind {
+                        if let RenameMode::To = rename_mode {
+                            amcm.data()
+                                .update_mod_file(String::from(path.to_str().unwrap()));
+                        }
+                    }
+                } else if let EventKind::Remove(remove_kind) = kind {
+                    if let RemoveKind::File = remove_kind {
+                        amcm.data()
+                            .remove_mod_file_from_filepath(String::from(path.to_str().unwrap()));
+                    } else if let RemoveKind::Any = remove_kind {
+                        amcm.data()
+                            .remove_mod_file_from_filepath(String::from(path.to_str().unwrap()));
+                    }
+                } else {
+                    println!("Event total cost: {:#?}", time_event_start.elapsed());
+                    println!("###### Event handle end #####");
+                    return;
+                }
+                println!("Update data cost: {:#?}", time_start.elapsed());
+
+                // Save data
+                let time_start = Instant::now();
+                amcm.save_data();
+                println!("Save data cost: {:#?}", time_start.elapsed());
+
+                // Emit
+                let time_start = Instant::now();
+                window
+                    .emit("mod_files_updated", amcm.data().mod_files())
+                    .expect("Event mod_files_updated emit failed");
+                println!("Emit cost: {:#?}", time_start.elapsed());
+
+                println!("Event total cost: {:#?}", time_event_start.elapsed());
+                println!("###### Notify Event handle end #####");
+            }
+            Err(e) => {
+                println!("watch error: {:?}", e);
+            }
+        }
+    }
 
     pub async fn watch_mod_files(&mut self, dir: String, window: Window) -> Result<(), String> {
-        // TODO: Optimize it
         self.data.update_mod_files(dir.clone()).await;
         window
             .emit("mod_files_updated", self.data.mod_files())
             .expect("Event mod_files_updated emit failed");
 
-        // let dir_ = dir.clone();
-        // let window_ = window.clone();
-        // if let Err(error) = self.hotwatch_watcher.watch(dir.clone(), move |event: Event| {
-        //     println!("###### Event handle start #####");
-        //     let time_event_start = Instant::now();
-        //     let mut amcm = futures::executor::block_on(async { CORE.lock().await });
-        //     println!("get amcm lock cost: {:#?}", time_event_start.elapsed());
-
-        //     let time_start = Instant::now();
-        //     println!("{:#?}", event);
-        //     if let Event::Create(filepath)= event {
-        //         amcm.data().update_mod_file(String::from(filepath.to_str().unwrap()));
-        //     } else if let Event::Rename(srcpath, dstpath) = event {
-        //         amcm.data().update_mod_file(String::from(dstpath.to_str().unwrap()));
-        //     } else if let Event::Remove(filepath) = event {
-        //         amcm.data().remove_mod_file_from_filepath(String::from(filepath.to_str().unwrap()));
-        //     } else {
-        //         println!("Event total cost: {:#?}", time_event_start.elapsed());
-        //         println!("###### Event handle end #####");
-        //         return;
-        //     }
-        //     println!("Update data cost: {:#?}", time_start.elapsed());
-        //     let time_start = Instant::now();
-        //     amcm.save_data();
-        //     println!("Save data cost: {:#?}", time_start.elapsed());
-        //     let time_start = Instant::now();
-        //     window_.emit("mod_files_updated", amcm.data().mod_files()).expect("Event mod_files_updated emit failed");
-        //     println!("Emit cost: {:#?}", time_start.elapsed());
-        //     println!("Event total cost: {:#?}", time_event_start.elapsed());
-        //     println!("###### Event handle end #####");
-        // }) {
-        //     return Err(error.to_string());
-        // }
-        // let dir_ = dir.clone();
-        // window.once("unwatch_mod_files", |_| {
-        //     let mut amcm = futures::executor::block_on(async { CORE.lock().await });
-        //     amcm.hotwatch_watcher.unwatch(dir_).expect("Mod files unwatch failed");
-        // });
-        let dir_ = dir.clone();
         let window_ = window.clone();
         match recommended_watcher(move |res: notify::Result<notify::Event>| {
-            use notify::{
-                event::{CreateKind, ModifyKind, RemoveKind, RenameMode},
-                EventKind,
-            };
-            match res {
-                Ok(event) => {
-                    println!("event: {:#?}", event);
-                    println!("###### Notify Event handle start #####");
-
-                    // Get amcm lock
-                    let time_event_start = Instant::now();
-                    let mut amcm = futures::executor::block_on(async { CORE.lock().await });
-                    println!("get amcm lock cost: {:#?}", time_event_start.elapsed());
-
-                    // Update mod file
-                    let time_start = Instant::now();
-                    println!("{:#?}", event);
-                    let path = event.paths.last().unwrap();
-                    let kind = event.kind;
-                    if let EventKind::Create(create_kind) = kind {
-                        if let CreateKind::File = create_kind {
-                            amcm.data()
-                                .update_mod_file(String::from(path.to_str().unwrap()));
-                        }
-                    } else if let EventKind::Modify(modify_kind) = kind {
-                        if let ModifyKind::Name(rename_mode) = modify_kind {
-                            if let RenameMode::To = rename_mode {
-                                amcm.data()
-                                    .update_mod_file(String::from(path.to_str().unwrap()));
-                            }
-                        }
-                    } else if let EventKind::Remove(remove_kind) = kind {
-                        if let RemoveKind::File = remove_kind {
-                            amcm.data().remove_mod_file_from_filepath(String::from(
-                                path.to_str().unwrap(),
-                            ));
-                        }
-                    } else {
-                        println!("Event total cost: {:#?}", time_event_start.elapsed());
-                        println!("###### Event handle end #####");
-                        return;
-                    }
-                    println!("Update data cost: {:#?}", time_start.elapsed());
-
-                    // Save data
-                    let time_start = Instant::now();
-                    amcm.save_data();
-                    println!("Save data cost: {:#?}", time_start.elapsed());
-
-                    // Emit
-                    let time_start = Instant::now();
-                    window_
-                        .emit("mod_files_updated", amcm.data().mod_files())
-                        .expect("Event mod_files_updated emit failed");
-                    println!("Emit cost: {:#?}", time_start.elapsed());
-
-                    println!("Event total cost: {:#?}", time_event_start.elapsed());
-                    println!("###### Notify Event handle end #####");
-                }
-                Err(e) => {
-                    println!("watch error: {:?}", e);
-                }
-            }
+            Core::notify_event_handler(res, window_.clone())
         }) {
             Ok(watcher) => self.notify_watcher = Some(watcher),
             Err(error) => {
                 return Err(error.to_string());
-            },
+            }
         }
 
         let path = Path::new(dir.as_str());
-        if let Err(error) = self.notify_watcher.as_mut().unwrap().watch(path, notify::RecursiveMode::NonRecursive) {
+        if let Err(error) = self
+            .notify_watcher
+            .as_mut()
+            .unwrap()
+            .watch(path, RecursiveMode::NonRecursive)
+        {
             return Err(error.to_string());
         }
 
-        // let dir_ = dir.clone();
         window.once("unwatch_mod_files", move |_| {
             let mut amcm = futures::executor::block_on(async { CORE.lock().await });
             let path = Path::new(dir.as_str());
-            amcm.notify_watcher.as_mut().unwrap()
+            amcm.notify_watcher
+                .as_mut()
+                .unwrap()
                 .unwatch(path)
                 .expect("Mod files unwatch failed");
         });
