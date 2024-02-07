282276cb2a1b58b5dd76354bfe6a623f51168bcc
Azur冰弦 <973562770@qq.com>
2024-02-07 21:27:24 +0800

    added flake

---
+++ flake.nix
@@ -0,0 +1,52 @@
+{
+  description = "azur-minecraft-manager";
+
+  nixConfig = {
+    extra-substituters = [
+      "https://mirrors.ustc.edu.cn/nix-channels/store"
+    ];
+    trusted-substituters = [
+      "https://mirrors.ustc.edu.cn/nix-channels/store"
+    ];
+  };
+
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    rust-overlay.url = "github:oxalica/rust-overlay";
+    flake-utils.url = "github:numtide/flake-utils";
+    nur.url = github:nix-community/NUR;
+  };
+
+  outputs = { self, nur, nixpkgs, rust-overlay, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        overlays = [ (import rust-overlay) (nur.overlay) ];
+        pkgs = import nixpkgs {
+          inherit system overlays;
+        };
+        rust-tools = pkgs.rust-bin.nightly."2022-10-09".default.override {
+          extensions = [ "rust-src" ];
+        };
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            clang
+            llvmPackages_16.bintools
+            # openssl
+            pkg-config
+          ] ++ [
+            rust-tools
+          ] ++ (with pkgs.darwin.apple_sdk.frameworks; pkgs.lib.optionals pkgs.stdenv.isDarwin [
+            System
+            IOKit
+            Security
+            CoreFoundation
+            AppKit
+            WebKit
+          ]);
+        };
+      }
+    );
+}
